3877f26fcb7c02c70822380b7420444fea9611f7
mm-ami <mhju.9599@gmail.com>
2022-09-15 20:26:06 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { Logo } from "./Logo"
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
+      <Grid minH="100vh">
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
           <Logo h="40vmin" pointerEvents="none" />
